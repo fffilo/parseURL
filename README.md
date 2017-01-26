ba61fb8c2678c0df679cbe642529e9e198062fe7
Franjo Filo <fffilo666@gmail.com>
2017-01-26 14:04:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+parseURL
+========
+
+work in progress...
---
+++ src/parseURL.js
@@ -0,0 +1,33 @@
+/**
+ * Parse URL
+ *
+ * @param  {String} url
+ * @return {Object}
+ */
+window.parseURL = function(url) {
+    var el = document.createElement("a");
+    el.href = url || document.location.href;
+
+    var result = {
+        href     : url,                // "http://domain.com:8080/path/to/somewhere/?foo=bar#hash"
+        protocol : el.protocol,        // "http:"
+        hostname : el.hostname,        // "domain.com"
+        port     : el.port || "80",    // "8080"
+        pathname : el.pathname,        // "/path/to/somewhere/"
+        search   : el.search,          // "?foo=bar"
+        data     : {},                 // { foo: "bar" }
+        hash     : el.hash,            // "#hash"
+        host     : el.host             // "domain.com:8080"
+    }
+
+    var arr = el.search.substring(1).split("&");
+    for(var i = 0; i < arr.length; i++) {
+        var kvl = arr[i].split("=");
+        var key = decodeURIComponent(kvl[0]);
+        var val = decodeURIComponent(kvl.slice(1).join("="));
+
+        result.data[key] = val;
+    }
+
+    return result;
+}
